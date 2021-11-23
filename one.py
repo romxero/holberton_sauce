f27376a848639e4744869d00f1b81f9c51beaf4c
_RC_ <randy.white.1986@gmail.com>
2021-11-23 17:00:12 -0600

    more pytorch work. almost done

--- one.py
+++ one.py
@@ -5,90 +5,67 @@
 ##
 
 
+from prompt_toolkit.layout import dimension
 import torch
 from torch import nn
 from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transforms import ToTensor, Lambda, Compose
 import numpy as np 
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt 
+import plotly as plty #plotly instead of seaborn 
+#import seaborn as sns ##deprecated 
 
 
-#lets define the batch size:
-batch_size = 64 
-
-#set up the devices right here 
-dtype = torch.float
-device = torch.device("cpu") # This executes all calculations on the CPU
-
-#create a tensor type
-x = torch.tensor([[1, 2, 3], [4, 5, 6]])
-
-
-#set the random seed state
-torch.random.seed()
-
-#generator 
-g_cpu = torch.Generator()
+#global variables that the student shouldn't need to set, or use 
+appNumOfCpu = 4 #set this variable here 
+batchSize = 64 #batch size 
+numOfEpochs = 200 #numbee of iterations for training
+learningRate = 0.0002 #learning rate lr
+learningRateV2 = 1e-3 #this is a different learning rate to try in the simulation..
+decayOfFirstOrderMom = 0.5 #first order b1
+decayOfSecondOrderMom = 0.999 # second order b2
+dimensionalityOfLatentSpace = 100 #name speaks for itself 
+stdImageSize = 28 #squared size of image dimension 
+numOfImageChannels = 1 #image channels 
+samplerInterval = 400 #interval between sampling
 
 
-#generator right here 
-def generator(Z):
-	#torch.tensor()
-# take a look at some other methods, that are below, instead of instantiating a sequential layer 
+#### End of global variables 
+
+#grab the MINST data set within pytorch:
+training_data = datasets.FashionMNIST(
+    root="data",
+    train=True,
+    download=True,
+    transform=ToTensor()
+)
+#grab training set and test set data
+test_data = datasets.FashionMNIST(
+    root="data",
+    train=False,
+    download=True,
+    transform=ToTensor()
+)
 
 
-    generatorModel = nn.Sequential(
-            nn.Linear(128),
-            nn.ReLU(),
-            nn.Linear(784),
-            nn.ReLU(),
-            nn.Sigmoid()
-          )
-    
-    # ~ self.model = nn.Sequential(
-            # ~ *block(opt.latent_dim, 128, normalize=False),
-            # ~ *block(128, 256),
-            # ~ *block(256, 512),
-            # ~ *block(512, 1024),
-            # ~ #nn.Linear(1024, int(np.prod(shape=(Z[0])))),
-            # ~ nn.ReLU()
-        )
+#add this with the data loader method 
+train_dataloader = DataLoader(training_data, batch_size=batchSize)
+test_dataloader = DataLoader(test_data, batch_size=batchSize)
 
 
-    return logits
-
-
-
-def discriminator(self, x):
-	x = self.flatten(x)
-    logits = self.linear_relu_stack(x)
-    return logits
-
-def train_discriminator(self, x):
-	return 0
-
-def train_generator(self, x):
-	return 0
-
-def sample_Z(self, x):
-	return 0
-
-def train_GAN(self, x):
-	return 0
-
-
+#probably not needed right here 
+discriminator_activation_function = torch.sigmoid
+generator_activation_function = torch.tanh
 
 
 
 # Add the loss functions here and optimizers here 
 loss_fn = nn.CrossEntropyLoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
-
 # Adversarial Loss function
 adversarial_loss = torch.nn.BCELoss()
 
-##Make sure to add backwards propagation down here: 
+#optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
 
 
 
@@ -97,44 +74,98 @@ adversarial_loss = torch.nn.BCELoss()
 
 
 
+#Remove below 
+#lets define the batch size:
+
+
+#set up the devices right here 
+#dtype = torch.float
+#device = torch.device("cpu") # This executes all calculations on the CPU
+
+#create a tensor type
+# DEL # x = torch.tensor([[1, 2, 3], [4, 5, 6]])
 
 
 
+#generator 
+g_cpu = torch.Generator()
 
-#this is the main file to create a generator in 
+
+
+## For the model you have to define a method right here
+class generatorNetwork(nn.Module):
+    def __init__(self):
+        super(generatorNetwork, self).__init__()
+        self.flatten = nn.Flatten() #don't think I need to flatten right here 
+        
+        # Inputs layer: 128
+        # Hidden layer: 784
+        self.hidden = nn.Linear(128, 784)
+        self.layer_1 = nn.Linear(128, 784) 
+        
+        
+        # Output layer, 10 units - one for each digit
+        self.output = nn.Linear(784, 10)
+        self.layer_2 = nn.Linear(784, 10)
+
+    def forward(self, x):
+        forwardOutputs = [0,0,0,0,0] #array slice here to test
+
+        #right here apply the first layer 
+        x = self.layer_1(x)
+        nn.ReLU()
+        #next activation function 
+
+        #doing this layer 2 right here
+        x = self.layer_2(x)
+        nn.Sigmoid() #sig
+        #logits = self.linear_relu_stack(x)
+        return forwardOutputs
+
+
+
+## For the model you have to define a method right here
+class descriminatorNetwork(nn.Module):
+    def __init__(self):
+        super(descriminatorNetwork, self).__init__()
+        self.flatten = nn.Flatten() #don't think flattening is needed 
+
+        # Inputs layer: 128
+        # Hidden layer: 784
+        self.hidden = nn.Linear(128, 128)
+        self.layer_1 = nn.Linear(128, 128) 
+        
+        
+        # Output layer, 10 units - one for each digit
+        self.output = nn.Linear(128, 1)
+        self.layer_2 = nn.Linear(128, 1)
+
+    def forward(self, x):
+        forwardOutputs = [0,0,0,0,0] #array slice here to test
+        x = self.layer_1(x)
+        nn.ReLU()
+        #next activation function 
+
+        #doing this layer 2 right here
+        x = self.layer_2(x)
+        nn.Sigmoid()
+        return forwardOutputs
+
+# The optimizers are here for the networks  
+# Optimizers
+optimizer_G = torch.optim.Adam(generator.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
+optimizer_D = torch.optim.Adam(discriminator.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
+
+
+
+###
+#Training
+torch.random.seed()
+
+for epochs in range(numOfEpochs):
+    print(epochs)
+
+
 ##
-##
-##
-##
 
 
-#lets download the training and test data from an online repository 
-
-# Download training data from open datasets.
-training_data = datasets.FashionMNIST(
-    root="data",
-    train=True,
-    download=True,
-    transform=ToTensor(),
-)
-
-# Download test data from open datasets.
-test_data = datasets.FashionMNIST(
-    root="data",
-    train=False,
-    download=True,
-    transform=ToTensor(),
-)
-
-
-
-#	encoder_layer = nn.TransformerEncoderLayer(d_model=512, nhead=8)
-#	transformer_encoder = nn.TransformerEncoder(encoder_layer, num_layers=6)
-#	src = torch.rand(10, 32, 512)
-#	out = transformer_encoder(src)
-
-	#this is the layer stuff below right here  
-#	layer_1 = nn.layer(128, activation='relu') #middle layer with relu activation
-#    layer_2 = nn.layer(784, activation='sigmoid') #last layer with sigmoid activation    
-# ~ __self.model = nn.Sequential(layer_1,layer_2,nn.ReLU()) #I think this should be the right syntax- its just psuedo code anyways 
-    
